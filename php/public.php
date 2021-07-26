540e2f534b8e3953d603b2fbd7d5520a4cbc08ff
yefei-yefei <80298453+yefei-yefei@users.noreply.github.com>
2021-07-26 15:50:40 +0800

    time
    time

--- php/public.php
+++ php/public.php
@@ -1,5 +1,5 @@
 <?php
-  //   $conn=new mysqli('localhost','root','','dream');
+    // $conn=new mysqli('localhost','root','','dream');
 	 
 	   $conn=new mysqli('localhost','root','x4AFdY3ki4f5rCJA','meng');
 	     $conn->query('set names utf8');
@@ -15,5 +15,5 @@
 	 	 }
 	 	 $URL="https://www.hd512.com/dream/images/";
 		 // // $URL='http://192.168.1.170/dream/php/images/';
-		 //  $URL='http://192.168.7.108/dream_to-vote/php/images/';
+		  // $URL='http://192.168.7.108/dream_to-vote/php/images/';
 ?>
